8d94709862affc4499feeed71f393812ed3a010e
Jordanedwardlwyaw <jlwaya@byupathwy.edu>
2025-05-07 12:29:06 +0300

    update

---
+++ Pratice.py
@@ -0,0 +1,13 @@
+# the importance of this execise is to memorize basic keys
+
+print("Welcome to my World")
+Age = 18
+input("What is your Age")
+
+if Age > 16: 
+    print("U are able to watch the movie!") 
+    input("Congradutions you are now an adult!")
+else:
+    print("Sorry this movie is not for views under the age of eighteen years!") 
+    input("Now go back home before i call your parents!")
+    
